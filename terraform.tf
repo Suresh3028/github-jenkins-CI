d95a42e26996a0d099df9e5924d4c4608729bdac
sureshkatta2830@gmail.com <naresh.naresh.k7@gmail.com>
2024-12-28 11:11:33 +0530

    creating ec2

---
+++ terraform.tf
@@ -0,0 +1,15 @@
+module "ec2_instance" {
+  source = "terraform-aws-modules/ec2-instance/aws"
+
+  name = "single-instance"
+
+  instance_type = "t2.micro"
+  key_name      = "user1"
+  monitoring    = true
+  ami           = "ami-01816d07b1128cd2d"
+
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
+  }
+}
